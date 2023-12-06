80e2a8fe1c156d98e881ad07e77224dde6360f3c
AimanFadil <aimanbello2001@gmail.com>
2023-12-07 00:24:36 +0100

    array of object

---
+++ js/script.js
@@ -0,0 +1,32 @@
+//SNACK 1
+let biciclette = [
+    {
+        nome: "Atala", 
+        peso: 10,
+    },
+    {
+        nome: "Basso", 
+        peso: 11.0
+    },
+    {
+        nome: "Bianchi", 
+        peso: 8.0
+    },
+    {
+        nome: "Fantic", 
+        peso: 10.0
+    },
+    {
+        nome: "Triestina", 
+        peso: 10,
+    },
+    {
+        nome: "Olympia", 
+        peso: 10,
+    },
+    {
+        nome: "Brinke Bike", 
+        peso: 10,
+    },
+];
+
